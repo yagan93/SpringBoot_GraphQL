421780c5071e62ba852580341e89093e4a82fefc
Basil <basil.peter@hotmail.ch>
2022-09-02 14:36:41 +0200

    added CustomerSpecifications

--- src/main/java/com/example/graphQL/domain/author/AuthorRepository.java
+++ src/main/java/com/example/graphQL/domain/author/AuthorRepository.java
@@ -1,10 +1,15 @@
 package com.example.graphQL.domain.author;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface AuthorRepository extends JpaRepository<Author, UUID> {
+public interface AuthorRepository extends JpaRepository<Author, UUID> , JpaSpecificationExecutor {
+
+  
 }
---
+++ src/main/java/com/example/graphQL/domain/author/specification/CustomerSpecifications.java
@@ -0,0 +1,14 @@
+package com.example.graphQL.domain.author.specification;
+
+
+import com.example.graphQL.domain.author.Author_;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerSpecifications<T> {
+    public Specification<T> hasAuthorFirstName(String firstName) {
+        return (root, query, builder) -> builder.equal(root.get(Author_.FIRST_NAME), firstName);
+    }
+}
